8311554516130cb31f58d10f17fcb5dc01a04d3c
carlos.vargas <carlos.vargas.7e8@itb.cat>
2025-05-16 14:04:48 +0200

    progreso del login y el register

--- app/src/main/java/com/example/mapsapp/ui/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/example/mapsapp/ui/navigation/NavigationWrapper.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mapsapp.ui.drawer.Destination
 import com.example.mapsapp.ui.drawer.DrawerScreens
+
 import com.example.mapsapp.ui.screens.MapScreen
 import com.example.mapsapp.ui.screens.PermissionsScreen
 
@@ -16,10 +18,10 @@ fun NavigationWrapper(){
     val navController = rememberNavController()
     NavHost(navController, Permisos){
         composable<Permisos>{
-            PermissionsScreen{  navController.navigate(Destination.Map) }
+            PermissionsScreen{  navController.navigate(Destination.Login) }
         }
-        composable<Destination.Map> {
-            DrawerScreens()
+        composable<Destination.Login> {
+            NavigationLogin()
             }
         }
 }
